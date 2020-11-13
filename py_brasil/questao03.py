6ed435d3e26e0f3441f9fca1b110c9c39922d7c2
leocassiosilva <leocassiosilva1234@gmail.com>
2020-11-13 00:39:44 -0300

    pythonBR

---
+++ py_brasil/questao03.py
@@ -0,0 +1,12 @@
+#Faça um Programa que peça dois números e imprima a soma.
+
+num1 = int(input("Entre com um numero: "))
+
+num2 = int(input("Entre com um numero: "))
+
+soma = num1 + num2;
+
+print("Primeiro numero digitado [%i] " %num1)
+print("Primeiro numero digitado [%i] " %num1)
+
+print("Soma entre: [%i] + [%i] = [%i]" %(num1, num2, soma))
