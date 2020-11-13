a640c331e46dde814f75b3aa6fede7d6ad2064a2
leocassiosilva <leocassiosilva1234@gmail.com>
2020-11-13 00:28:57 -0300

    pythonBR

---
+++ py_brasil/questao01.py
@@ -0,0 +1,6 @@
+#Faça um Programa que peça um número e então mostre a mensagem O número informado foi [número].
+
+num = int(input("Entre com um numero: \n"))
+
+print("O numero informado foi [%i]" %(num))
+
