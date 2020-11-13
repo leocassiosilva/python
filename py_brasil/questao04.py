e45f288ee580d3d6843fb181460e782124fd62b3
leocassiosilva <leocassiosilva1234@gmail.com>
2020-11-13 00:48:23 -0300

    pythonBR

---
+++ py_brasil/questao04.py
@@ -0,0 +1,13 @@
+#Faça um Programa que peça as 4 notas bimestrais e mostre a média.
+
+nota1 = int(input("Entre com a primeira nota: "))
+
+nota2 = int(input("Entre com a segunda nota: "))
+
+nota3 = int(input("Entre com a terceira nota: "))
+
+nota4 = int(input("Entre com a quarta nota: "))
+
+media =(nota1+nota2+nota3+nota4)/4 
+
+print("A media é : %.2f" %media)
