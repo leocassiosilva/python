20ed2db7f2ee70a5218ab00947903951bd7d7e2a
leocassiosilva <leocassiosilva1234@gmail.com>
2020-11-15 01:34:01 -0300

    questao05

---
+++ py_brasil/questao05.py
@@ -0,0 +1,6 @@
+#Faça um Programa que converta metros para centímetros.
+metros = float(input("Entre com o valor em metros: "))
+
+centimetro = metros * 100
+
+print("Valor em centimetros [%.2f]" %centimetro)
