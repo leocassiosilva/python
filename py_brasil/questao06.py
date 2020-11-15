d0182b1f6076ab438a15db40b112b321d2aa16b4
leocassiosilva <leocassiosilva1234@gmail.com>
2020-11-15 01:55:12 -0300

    questao05

---
+++ py_brasil/questao06.py
@@ -0,0 +1,6 @@
+#Faça um Programa que peça o raio de um círculo, calcule e mostre sua área.
+raio = float(input("Entre com o raio: "))
+
+area = (3.14 *(raio*raio))
+
+print("O valor da area é: [%.2f]"%area)
