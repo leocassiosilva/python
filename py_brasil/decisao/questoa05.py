b848e02466067496efb9f7201b8da9d3b9568c7a
leocassiosilva <leocassiosilva1234@gmail.com>
2020-11-19 22:43:21 -0300

    questao 05

---
+++ py_brasil/decisao/questoa05.py
@@ -0,0 +1,10 @@
+#Faça um Programa que verifique se uma letra digitada é vogal ou consoante.
+
+
+letra = input("Entre com uma letra")
+
+if letra == "a" or letra == "e" or letra == "i" or letra == "o" or letra == "u":
+    print("A letra digitada é uma vogal")
+else: 
+    print("A letra digitada é uma consoante")
+    
