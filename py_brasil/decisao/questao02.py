6697a690469310df4b568ab81a2d140b6aa2455d
leocassiosilva <leocassiosilva1234@gmail.com>
2020-11-19 22:24:07 -0300

    verificando se um numero é positivo

---
+++ py_brasil/decisao/questao02.py
@@ -0,0 +1,9 @@
+#Faça um Programa que peça um valor e mostre na tela se o valor é positivo ou negativo.
+
+num = int(input("Entre com um numero"))
+
+if num >= 0: 
+    print("O numero %d é positivo" %num)
+else:
+    print("O numero %d é negativo" %num)
+
