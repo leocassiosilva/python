ffba5a6b8e47b01f91e6a40ebbe798383d66119b
leocassiosilva <leocassiosilva1234@gmail.com>
2020-11-23 21:04:56 -0300

    exemplo açaí

--- Questoes_Slide/exemplo_acai.py
+++ Questoes_Slide/exemplo_acai.py
@@ -5,4 +5,7 @@ while i<=3:
     qtd_acai = int(input("Entre com a quantidade de caçais vendidos: "))
     total = qtd_acai * 3.00
     print("Foram vendidos R$ %.2f de açaí. "%total)
+    total_geral=total_geral+total
     i= i+1
+
+    print("Foram vendidos R$ %.2f de açaí nos %d dias." %(total_geral, i-1))
