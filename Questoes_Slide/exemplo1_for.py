acd04e83eb34467c14c4cd5d076b2f5e8195362e
leocassiosilva <leocassiosilva1234@gmail.com>
2020-11-23 21:24:01 -0300

    exemplo for

---
+++ Questoes_Slide/exemplo1_for.py
@@ -0,0 +1,4 @@
+for i in range(1, 4):
+    qtd_acai = int(input("Entre com a quantidade de açais vendidos %d dia(s) atrás: " %i))
+    total = qtd_acai * 3.00
+    print("Foram comprados R$ %.2f de açaí. " %total)
