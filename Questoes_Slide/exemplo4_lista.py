5cae06f4098a05808b45cd73d74e5cbb269e054c
leocassiosilva <leocassiosilva1234@gmail.com>
2020-11-26 20:04:10 -0300

    exemplo2 lista

---
+++ Questoes_Slide/exemplo4_lista.py
@@ -0,0 +1,14 @@
+#Adicionar elementos em uma lista 
+
+umaLista = []
+
+umaLista.append("Lais")
+
+umaLista.append("Chico de Joãozinho")
+
+umaLista.append("Antônio")
+umaLista.append("Zefinha")
+i=0
+while i < 4:
+    print(umaLista[i])
+    i=i+1
