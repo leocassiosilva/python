cce06949f5ee48487ae2114a0e613d2040961858
leocassiosilva <leocassiosilva1234@gmail.com>
2020-11-28 07:40:05 -0300

    exemplo7

---
+++ Questoes_Slide/exemplo7_lista.py
@@ -0,0 +1,14 @@
+#remover o elemento e retornar o elemento removido
+#utilizando o 'pop'
+
+suaLista = [10, 50, 5, 2, 100] 
+
+elementoRemovido = suaLista.pop(2)
+print("Removido : %d " %elementoRemovido)
+
+i=0
+while i < 4: 
+    print(suaLista[i])
+    i=i+1
+
+    
