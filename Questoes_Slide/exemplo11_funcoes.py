3c688d0a70c96414a60a70d8f4833aee125cd456
leocassiosilva <leocassiosilva1234@gmail.com>
2020-11-29 13:48:58 -0300

    exemplo11_funcao

---
+++ Questoes_Slide/exemplo11_funcoes.py
@@ -0,0 +1,9 @@
+#Função para calcular o quadrado de um numero
+
+#função
+def quadrado(numero):
+    return numero * numero
+
+
+num = int(input("Entre com um numero: "))
+print("%d\t" %quadrado(num))
